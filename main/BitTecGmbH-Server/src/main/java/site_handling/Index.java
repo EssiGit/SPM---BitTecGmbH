cead7dbfa846a4c6b0fc13a0a84bc2cabe771515
yoon <da6332@hs-el.de>
2023-04-25 17:14:29 +0200

    added server and thymeleaf

---
+++ main/BitTecGmbH-Server/src/main/java/site_handling/Index.java
@@ -0,0 +1,28 @@
+package site_handling;
+
+
+import java.io.IOException;   
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import server_conf.ThymeleafConfig;
+import org.thymeleaf.context.WebContext;
+
+
+@WebServlet("/index")
+public class Index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        WebContext context = new WebContext(request, response,
+                request.getServletContext());
+        response.setCharacterEncoding("UTF-8");
+        ThymeleafConfig.getTemplateEngine().process("Index.html", context, response.getWriter());
+    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
